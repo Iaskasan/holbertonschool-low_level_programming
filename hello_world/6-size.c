00220ba68f5b1509c58af5b3c032de00c871dc4a
Iaskasan <nokiesan@yahoo.fr>
2023-10-12 06:31:58 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,12 +8,10 @@
 
 int main(void)
 {
-printf("Size of char: %lu bytes\n", sizeof(char));
-printf("Size of int: %lu bytes\n", sizeof(int));
-printf("Size of long: %lu bytes\n", sizeof(long));
-printf("Size of long long: %lu bytes\n", sizeof(long long));
-printf("Size of float: %lu bytes\n", sizeof(float));
-printf("Size of double: %lu bytes\n", sizeof(double));
-printf("Size of long double: %lu bytes\n", sizeof(long double));
+printf("Size of a char: %lu bytes\n", sizeof(char));
+printf("Size of an int: %lu bytes\n", sizeof(int));
+printf("Size of a long int: %lu bytes\n", sizeof(long));
+printf("Size of a long long int: %lu bytes\n", sizeof(long long));
+printf("Size of a float: %lu bytes\n", sizeof(float));
 return (0);
 }

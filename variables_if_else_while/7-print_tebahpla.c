ce90f92cb3f08998f20a2e41aa3288549c141711
Iaskasan <nokiesan@yahoo.fr>
2023-10-16 06:10:46 -0700

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+* Description: 2. I sometimes suffer from insomnia. And when I can't fall
+* asleep, I play what I call the alphabet game
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a' ; c--)
+{
+	putchar (c);
+}
+{
+	putchar('\n');
+}
+	return (0);
+}

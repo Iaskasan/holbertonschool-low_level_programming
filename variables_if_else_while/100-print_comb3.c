3c7cc13932e175636cadd74fd624193297535041
Iaskasan <nokiesan@yahoo.fr>
2023-10-16 12:18:56 -0700

    100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+* Description: 2. I sometimes suffer from insomnia. And when I can't fall
+* asleep, I play what I call the alphabet game
+*/
+int main(void)
+{
+	int n1, n2, n4;
+	int n3 = 89;
+
+	for (n1 = '0' ; n1 <= '9' ; n1++)
+	for (n2 = '1' ; n2 <= '9' ; n2++)
+		if (n1 != n2)
+		if (n1 < n2)	
+{
+	putchar (n1);
+	putchar (n2);
+	if (n1 != '8')
+{
+	putchar (',');
+	putchar (' ');
+}
+}
+{
+	putchar('\n');
+}
+	return (0);
+}

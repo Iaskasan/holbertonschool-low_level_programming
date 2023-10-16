3eceab005f9a912f6f59f75e792cb00ba53bb19d
Iaskasan <nokiesan@yahoo.fr>
2023-10-16 06:38:24 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+* Description: 2. I sometimes suffer from insomnia. And when I can't fall
+* asleep, I play what I call the alphabet game
+*/
+int main(void)
+{
+	int c;
+
+	for (c = '0' ; c <= '9' ; c++)
+{
+	putchar (c);
+
+
+	if (c != 9)
+{
+	putchar (',');
+	putchar (' ');
+}
+}
+	return (0);
+}

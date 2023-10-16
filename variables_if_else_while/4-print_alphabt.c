95c9b6e9f37290bf7c36e0f6e398cdfcf58df614
Iaskasan <nokiesan@yahoo.fr>
2023-10-16 05:48:39 -0700

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+* Description: 2. I sometimes suffer from insomnia. And when I can't fall
+* asleep, I play what I call the alphabet game
+*/
+int main(void)
+{
+	char c;
+        for (c = 'a' ; c <= 'z' ; c++)
+	if (c != 'q' && c != 'e')
+{
+	putchar (c);
+}
+{
+	putchar('\n');
+}
+	return (0);
+}

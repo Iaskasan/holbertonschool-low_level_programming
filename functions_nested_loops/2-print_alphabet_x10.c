cae0536413367f06c90a68ba31d90e63020a53b4
Iaskasan <nokiesan@yahoo.fr>
2023-10-17 05:26:02 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_alphabet - yolo
+* print_alphabet - prototype
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void);
+{
+
+	char i;
+	int iter = 10
+
+	for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+{
+	while (i < iter)
+{
+	iter;
+	i++
+}
+}
+	_putchar('\n');
+}

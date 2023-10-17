d25e6406fe81a89aa30cbbc74a835036d019d3c5
Iaskasan <nokiesan@yahoo.fr>
2023-10-17 05:51:04 -0700

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,17 @@
 * Return: Always 0 (Success)
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char i;
-	int iter = 10;
+	int numRepetitions = 10;
 
-	for (int count = 0; count < iter; count++)
+	for (int repetition = 0; repetition < numRepetitions; repetition++)
+{
 	for (i = 'a'; i <= 'z'; i++)
 {
 	_putchar(i);
 }
 	_putchar('\n');
-	i++;
+}
 }

ee8281799e7b26788fdc7c1516616f4f7f5e3aa2
Iaskasan <nokiesan@yahoo.fr>
2023-10-17 07:02:35 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - returns 1 if character is lowercase else 0
+* @c: The character to be checked.
+* Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') && (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}

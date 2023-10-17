48560f4110b50d1b4e9ab76c46358abc3bc598ef
Iaskasan <nokiesan@yahoo.fr>
2023-10-17 07:50:13 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _abs - returns 1 if character is lowercase else 0
+* @x: The character to be checked.
+* Return: Always 0 (Success)
+*/
+
+int _abs(int x)
+{
+{
+	if (x < 0)
+{
+	return (-x);
+}
+	else
+{
+	return (x);
+}
+}
+	return (0);
+}

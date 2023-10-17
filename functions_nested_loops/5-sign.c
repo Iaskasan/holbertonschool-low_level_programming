ff511189b900b0c32d76fb540b9aceda9272e94e
Iaskasan <nokiesan@yahoo.fr>
2023-10-17 07:16:53 -0700

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - returns 1 if character is lowercase else 0
+* @n: The character to be checked.
+* Return: Always 0 (Success)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+{
+	_putchar('+');
+}
+	return (1);
+	if (n == 0)
+{
+	_putchar('0');
+}
+	return (0);
+	if (n < 0)
+{
+	_putchar('-');
+}
+	return (-1);
+}

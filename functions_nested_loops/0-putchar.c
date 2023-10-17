f55053d192d0b2feb5836605c092449910fc2047
Iaskasan <nokiesan@yahoo.fr>
2023-10-17 03:08:46 -0700

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * main
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	char str[20] = "_putchar";
+
+	for (int i = 0 ; i <= 8 ; i++)
+{
+	putchar(str[i]);
+}
+	return (0);
+}

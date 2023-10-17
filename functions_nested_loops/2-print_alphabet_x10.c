be1fb91e3a7e61cfddaed0888980c011fd301a4f
Iaskasan <nokiesan@yahoo.fr>
2023-10-17 05:45:51 -0700

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,21 @@
 #include "main.h"
 
 /**
-* print_alphabet - yolo
-* print_alphabet - prototype
+* print_alphabet_x10 - yolo
+* print_alphabet_x10 - prototype
 * Return: Always 0 (Success)
 */
 
 void print_alphabet_x10(void);
 {
-
 	char i;
-	int iter = 10
+	int iter = 10;
 
+	for (int count = 0; count < iter; count++)
 	for (i = 'a'; i <= 'z'; i++)
 {
 	_putchar(i);
 }
-{
-	while (i < iter)
-{
-	iter;
-	i++
-}
-}
 	_putchar('\n');
+	i++;
 }

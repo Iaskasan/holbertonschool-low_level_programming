aecccd8f7285cf7ab54816707b6f1b0b67250928
Iaskasan <nokiesan@yahoo.fr>
2023-10-18 01:49:27 -0700

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-* print_last_digit - returns 1 if character is lowercase else 0
-* @x: The character to be checked.
-* Return: Always 0 (Success)
+* print_last_digit - prints the last digit of a number
+* @x: is the modulo 10
+* Return: Returns the last digit of x
 */
 
 int print_last_digit(int x)
 {
-	int xLast = x % 10;
+	int xLast;
+
+	xLast = x % 10;
 {
-	_putchar (0 + xLast);
+	_putchar ('0' + xLast);
 }
 	return (xLast);
 }

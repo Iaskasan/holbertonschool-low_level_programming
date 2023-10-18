b77856a2584662f4d87d89caada4f2eb03e101b6
Iaskasan <nokiesan@yahoo.fr>
2023-10-18 01:27:39 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_last_digit - returns 1 if character is lowercase else 0
+* @x: The character to be checked.
+* Return: Always 0 (Success)
+*/
+
+int print_last_digit(int x)
+{
+	x = x % 10;
+{
+	_putchar(x);
+}
+	return (0);
+}

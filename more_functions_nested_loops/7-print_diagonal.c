14cd56ce42b3c5e43aaa59851b190c644f298510
Iaskasan <nokiesan@yahoo.fr>
2023-10-20 05:59:49 -0700

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,15 @@ void print_diagonal(int n)
 
 {
 	int a, b;
-
+if (n <= 0)
+_putchar('\n');
+	else
 	for (a = 1 ; a <= n ; a++)
 	{
 		for (b = 0 ; b < a ; b++)
 			if (a > b + 1)
 			_putchar(' ');
 		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

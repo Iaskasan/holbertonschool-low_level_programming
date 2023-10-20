497935732e3907bdc4eb5ecab238e82f0c555135
Iaskasan <nokiesan@yahoo.fr>
2023-10-20 02:36:55 -0700

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_most_numbers - print_most_numbers
+*
+*/
+
+void print_most_numbers(void)
+{
+	int print_most_numbers;
+
+	if (print_most_numbers != 2 && print_most_numbers != 4)
+	for (print_most_numbers = 0; print_most_numbers <= 9 ; print_most_numbers++)
+	_putchar(print_numbers + '0');
+	_putchar('\n');
+}

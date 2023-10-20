72fa11ebeda7044c777987c2e232c899608c9164
Iaskasan <nokiesan@yahoo.fr>
2023-10-20 04:05:42 -0700

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,10 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14 ; i++)
 		{
-			putchar(i / 10 + '0');
 			if (i > 9)
-				putchar(i % 10 + '0');
+			_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
 		}
-		putchar('\n');
+	_putchar('\n');
 	}
 }

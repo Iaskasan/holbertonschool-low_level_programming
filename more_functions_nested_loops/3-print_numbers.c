f908a6983e6be921679983582cc366a3f9c9d20b
Iaskasan <nokiesan@yahoo.fr>
2023-10-20 02:19:01 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers
+*
+*/
+
+void print_numbers(void)
+{
+	int print_numbers;
+
+	for (print_numbers = 0; print_numbers <= 9 ; print_numbers++)
+	_putchar(print_numbers + '0');
+	_putchar('\n');
+}

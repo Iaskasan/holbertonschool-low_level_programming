dd1444f4f0eefe52c883eac04f129edf176840a1
Iaskasan <nokiesan@yahoo.fr>
2023-10-20 05:44:05 -0700

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal
+ * @n: number of lines
+ */
+
+void print_diagonal(int n)
+
+{
+	int a, b;
+
+	for (a = 1 ; a <= n ; a++)
+	{
+		for (b = 0 ; b < a ; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar ('\n');
+	}
+}

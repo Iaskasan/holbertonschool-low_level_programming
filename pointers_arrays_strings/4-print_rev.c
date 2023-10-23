7eaffaa851424053fd16bab1e4b0e22340d48f91
Iaskasan <nokiesan@yahoo.fr>
2023-10-23 06:01:09 -0700

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: a string
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+	a++;
+	}
+		while (s[a] >= 0)
+		{
+		_putchar(s[a]);
+		a--;
+		}
+	_putchar('\n');
+}

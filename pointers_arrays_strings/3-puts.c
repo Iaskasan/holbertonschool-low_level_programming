a56ba09f7eac3ab2670490b1e1264ba641ddb7ba
Iaskasan <nokiesan@yahoo.fr>
2023-10-23 04:59:41 -0700

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: a string
+ */
+
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+		_putchar(len + '0');
+}

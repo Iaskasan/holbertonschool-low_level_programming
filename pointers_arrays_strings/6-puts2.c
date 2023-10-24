8938b6b62b422472811caaf6b9da6a2016d3891d
Iaskasan <nokiesan@yahoo.fr>
2023-10-24 02:33:28 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char of a string
+ * @str: the string
+ *
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+	if (a % 2 == 0)
+	{
+	_putchar(str[a]);
+	}
+	a++;
+	}
+}

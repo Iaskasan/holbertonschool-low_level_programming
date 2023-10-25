9656e390d97c64f36ddfe58a21701cc876da0e0a
Iaskasan <nokiesan@yahoo.fr>
2023-10-25 05:27:24 -0700

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int a = 0;
 
-	while (dest[a] != '\0' && dest[a] < n)
+	while (dest[a] <= '\0' && dest[a] < n)
 	{
 		dest[a] = src[i];
 		i++;
 		a++;
 	}
-	dest[a] = '\0';
 	return (dest);
 }

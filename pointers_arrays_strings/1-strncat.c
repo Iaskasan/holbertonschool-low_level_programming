4266c4e4158ba673f27ae7faf83a9be0c15a4b8a
Iaskasan <nokiesan@yahoo.fr>
2023-10-25 02:02:14 -0700

    1.1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of src char to be printed
+ * Return: returns the concatenated string to *dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int destLen = 0;
+
+	while (dest[destLen] != '\0')
+	{
+	destLen++;
+	}
+	if (i <= n)
+	while (src[i] != '\0')
+	{
+	dest[destLen + i] = src[i];
+	i++;
+	}
+dest[destLen + i] = '\0';
+return (dest);
+}

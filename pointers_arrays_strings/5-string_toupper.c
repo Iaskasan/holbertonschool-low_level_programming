a48958ecbccdc499770c756765d20e2ae85de7b3
Iaskasan <nokiesan@yahoo.fr>
2023-10-26 06:09:37 -0700

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - Transform loercases char into uppercases
+ * @ado: the string
+ * Return: returns the string in uppercase
+ */
+
+char *string_toupper(char *ado)
+{
+	int i;
+
+
+	for (i = 0 ; i < *ado ; i++)
+	if (*ado < 123 && *ado > 97)
+		ado = ado - 32;
+	return (ado);
+}

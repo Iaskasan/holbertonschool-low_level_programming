56cbbd64a8ab8156a94f19ed6905cfc635b37d4e
Iaskasan <nokiesan@yahoo.fr>
2023-10-31 07:41:08 -0700

    ado

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Calculate the integer square root of a number.
- * _sqrt - Auxiliary function for computing the square root.
+ * _sqrt - Calculates the integer square root of a number using recursion.
  * @n: The number for which we want to find the square root.
  * @i: Auxiliary variable for testing square of numbers.
+ *
  * Return: The integer square root of n, or -1 if it is not an integer.
  */
-
 int _sqrt(int n, int i)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-
 	else if (i * i > n)
 	{
 		return (-1);
 	}
-
 	else if (i * i == n)
 	{
 		return (i);
@@ -28,7 +25,15 @@ int _sqrt(int n, int i)
 	return (_sqrt(n, i + 1));
 }
 
+/**
+ * _sqrt_recursion - Calculate the integer square root
+ * of a number using recursion.
+ * @n: The number for which we want to find the square root.
+ *
+ * Return: The integer square root of n, or -1 if it is not an integer.
+ */
 int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
+

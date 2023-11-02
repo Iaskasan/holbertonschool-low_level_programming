ffc1c52d1393f9a9efaf9bcccba13da490a369ed
Iaskasan <nokiesan@yahoo.fr>
2023-11-02 07:09:38 -0700

    test

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: oui oui
+ * @argv: argv
+ * Return: returns 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}

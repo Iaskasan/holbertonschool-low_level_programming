4250a891e8c5a0d8d6ce966b696c9b464238bc57
Iaskasan <nokiesan@yahoo.fr>
2023-11-02 07:29:00 -0700

    test

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: argc
+ * @argv: argv
+ * Return: returns 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (**argv != '\0')
+	{
+	printf("%s\n", argv[i]);
+	i++;
+	}
+	return (0);
+}

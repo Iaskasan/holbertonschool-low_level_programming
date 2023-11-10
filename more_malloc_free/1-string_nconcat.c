17e247d6b7020a2cf46bdd353aa11956c97e2040
Iaskasan <nokiesan@yahoo.fr>
2023-11-10 01:38:54 -0800

    nconc

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates n bytes of a given string
+ * @s1: yolo
+ * @s2: yolo
+ * @n: yolo
+ * Return: returns the concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *nconc;
+	int len1 = 0;
+	int len2 = 0;
+	int i = 0;
+	unsigned int j;
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	nconc = malloc(sizeof(char) * (len1 + n + 1));
+	if (nconc == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < len1; i++)
+	{
+		nconc[i] = s1[i];
+	}
+	for (j = 0 ; j < n; j++)
+	{
+		nconc[len1 + j] = s2[j];
+	}
+	n = len2;
+	nconc[len1 + n] = '\0';
+	return (nconc);
+}

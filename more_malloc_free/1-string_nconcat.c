3fd835f558f6ac62b2472b508c746be25d9b3246
Iaskasan <nokiesan@yahoo.fr>
2023-11-10 01:46:15 -0800

    nconc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -39,10 +39,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		nconc[len1 + j] = s2[j];
 	}
-	if (n >= j)
-	{
-	n = len2;
-	}
-	nconc[len1 + n] = '\0';
+	nconc[len1 + j] = '\0';
 	return (nconc);
 }

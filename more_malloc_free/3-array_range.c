9b9815d90af585970430e31991f617e275bdbba8
Iaskasan <nokiesan@yahoo.fr>
2023-11-10 03:52:10 -0800

    array range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0 ; i >= min && i <= max ; i++)
+	for (i = 0 ; i <= (max - min) ; i++)
 	{
 	arr[i] = min + i;
 	}

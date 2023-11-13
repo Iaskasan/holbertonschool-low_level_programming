a5609210cecf6c5bb635b2dd0906d25aad238c24
Iaskasan <nokiesan@yahoo.fr>
2023-11-13 01:35:12 -0800

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct my_struct - 
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct my_struct
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

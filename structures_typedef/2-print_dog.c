d7a3814ac3ab2cb7078d0a301b2716b538bc6777
Iaskasan <nokiesan@yahoo.fr>
2023-11-13 06:36:43 -0800

    test

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "Name: (nil)");
 	printf("Age: %.2f\n", d->age);
 	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }

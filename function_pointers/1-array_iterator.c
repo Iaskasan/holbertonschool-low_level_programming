f6f7a16d45f3fbd6b08e4ba16a595cffa9290e83
Iaskasan <nokiesan@yahoo.fr>
2023-11-15 01:52:05 -0800

    array iterator, removing size condition

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -18,7 +18,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array == NULL)
+	if (size == 0 || array == NULL)
 		return;
 
 	for (i = 0 ; i < size ; i++)

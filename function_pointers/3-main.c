4fc72677a3b0b401a53be7ac7057379a3b74f2c1
Iaskasan <nokiesan@yahoo.fr>
2023-11-16 05:34:11 -0800

    num1

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	}
 	if (operation != NULL)
 	{
-		if ((*operator == '/' || *operator == '%') && (num2 == 0 || num1 == 0))
+		if ((*operator == '/' || *operator == '%') && num2 == 0)
 		{
 			printf("Error\n");
 			return (100);

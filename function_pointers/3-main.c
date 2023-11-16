223932101f96012505e53c6cd526d7c8385c9500
Iaskasan <nokiesan@yahoo.fr>
2023-11-16 01:47:33 -0800

    gazguge

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	}
 	if (operation != NULL)
 	{
-		if ((*operator == '/' || *operator == '%') && num2 == 0)
+		if ((*operator == '/' || *operator == '%') && (num2 == 0 || num1 == 0))
 		{
 			printf("Error\n");
 			return (100);
@@ -37,4 +37,3 @@ int main(int argc, char *argv[])
 		return (99);
 	}
 }
-

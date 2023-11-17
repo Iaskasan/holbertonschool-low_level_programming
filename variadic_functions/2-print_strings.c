365625ad875c4d92fb871edc2b10830c994737ab
Iaskasan <nokiesan@yahoo.fr>
2023-11-17 04:10:31 -0800

    (nil) if str is NULL with continue

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 		{
 			printf("(nil)");
-			return;
+			continue;
 		}
 		printf("%s", str);
 		if (separator != NULL && i < n - 1)

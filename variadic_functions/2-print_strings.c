3c5e8877284a51befd9fbd592807f250f82bb338
Iaskasan <nokiesan@yahoo.fr>
2023-11-17 04:04:56 -0800

    l'exe marche donc allez le checkeur

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @n: the number of parameters
+ * @separator: the separators between strings (if any)
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		str = va_arg(args, char*);
+		printf("%s", str);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}

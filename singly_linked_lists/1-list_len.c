48124121e91454fd14d9c7b5b3150591cf8b1777
Iaskasan <nokiesan@yahoo.fr>
2023-11-29 18:48:18 +0100

    added description for 1-list_len file

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: Pointer to the first node of the list.
+ *
+ * Description: Iterates through each node of the list and increments
+ * a counter for each node until the end of the list is reached.
+ * Return: The total number of nodes in the list.
+ */
 
 size_t list_len(const list_t *h)
 {

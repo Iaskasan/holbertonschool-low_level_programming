f852ee16a3ab640ec2e0a93c3b863d90561403ef
Iaskasan <nokiesan@yahoo.fr>
2023-11-30 17:25:54 +0100

    added betty documentation do the file 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints the elements of a doubly linked list.
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * Return: The number of elements printed.
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {

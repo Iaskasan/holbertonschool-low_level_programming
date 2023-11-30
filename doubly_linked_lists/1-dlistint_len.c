66591d75ff1449dd9f020ba2dc914781e7596035
Iaskasan <nokiesan@yahoo.fr>
2023-11-30 17:45:45 +0100

    added betty style doc to 1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - Counts the number of elements of a list
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * Return: The number of elements in the list.
+ */
 
 size_t dlistint_len(const dlistint_t *h)
 {

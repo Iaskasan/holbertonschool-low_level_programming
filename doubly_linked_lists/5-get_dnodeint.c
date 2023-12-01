c5feab49ae548ce2c015f5704400bad547af4be8
Iaskasan <nokiesan@yahoo.fr>
2023-12-01 14:07:46 +0100

    changed the index starting point

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head == NULL || index < 1)
+	if (head == NULL || index < 0)
 	return (NULL);
 
 	while (head)

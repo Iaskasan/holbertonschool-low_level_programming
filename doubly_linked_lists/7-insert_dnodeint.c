7b5bf5f7b3bd5324f965dcb2d240bb90c8a066ff
Iaskasan <nokiesan@yahoo.fr>
2023-12-01 16:45:14 +0100

    betty...

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -40,7 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		free(new_node);
 		return (NULL);
-    }
+	}
 
 	new_node->next = current->next;
 	new_node->prev = current;

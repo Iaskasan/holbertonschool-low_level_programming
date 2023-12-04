d34cd26ff6bd3f6727d3fc31effd40be0b542c7c
Iaskasan <nokiesan@yahoo.fr>
2023-12-04 16:01:56 +0100

    uhdazd

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The NULL terminated string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         O/w - 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i = 0, w;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
+
+		w = write(fd, text_content, i);
+
+		if (w == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}

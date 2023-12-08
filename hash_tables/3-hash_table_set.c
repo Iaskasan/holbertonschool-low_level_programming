6b2bc01ae9fbc0b3616f97fa76f86d0ecc70e863
Iaskasan <nokiesan@yahoo.fr>
2023-12-08 05:22:39 -0800

    removed some lines

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
+
 	hash_node_t *new_node = NULL, *temp = NULL;
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);

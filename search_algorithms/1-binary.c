ed35e72d49d179818315a8d504c2a2102f350ff6
Iaskasan <nokiesan@yahoo.fr>
2024-01-17 17:57:36 +0100

    added the print loop

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -15,12 +15,23 @@ int binary_search(int *array, size_t size, int value)
 	int left = 0;
 	int right = size - 1;
 	int mid;
+	int i;
 
 	if (array == NULL)
 		return (-1);
 
 	while (left <= right)
 	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
 		mid = left + (right - left) / 2;
 
 		if (array[mid] == value)
